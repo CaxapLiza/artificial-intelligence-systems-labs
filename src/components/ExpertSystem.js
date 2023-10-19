f726c91dfe8b50947aabae91b648c0d6968e1b19
lizac <lizacaxap@yandex.ru>
2023-10-19 16:02:56 +0300

    Add Node, Edge, Relationship, Graph classes

--- src/components/ExpertSystem.js
+++ src/components/ExpertSystem.js
@@ -1,6 +1,91 @@
 import {useState} from "react";
 
+class Node {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id
+  }
+
+  toString() {
+    return this.name;
+  }
+}
+
+class Relationship {
+  constructor(name, id) {
+    this.name = name;
+    this.id = id;
+  }
+
+  toString() {
+    return this.name;
+  }
+}
+
+class Edge {
+  constructor(start, end, relationship) {
+    this.start = start;
+    this.end = end;
+    this.relationship = relationship;
+  }
+
+  toString() {
+    return this.start + " " + this.relationship + " " + this.end;
+  }
+}
+
+class Graph {
+  constructor(edges) {
+    this.edges = edges
+  }
+
+  findAllPathsDFS(currentNode, endNode, visited, currentPath, allPaths) {
+    visited[currentNode.id] = true;
+
+    this.edges.forEach((edge) => {
+      if (edge.start === currentNode && !visited[edge.end.id])
+      {
+        currentPath.add(edge);
+
+        if (edge.end === endNode)
+        {
+          allPaths.add(currentPath);
+        }
+        else
+        {
+          this.findAllPathsDFS(edge.end, endNode, visited, currentPath, allPaths);
+        }
+        currentPath.remove(edge);
+      }
+    })
+
+    visited[currentNode.id] = false;
+  }
+
+  findAllPaths(startNode, endNode) {
+    const allPaths = [];
+    const currentPath = [];
+    const visited = [];
+
+    this.findAllPathsDFS(startNode, endNode, visited, currentPath, allPaths);
+
+    return allPaths;
+  }
+}
+
 function ExpertSystem() {
+
+  //consts
+  const taskNumber = 0;
+  const nodes = [];
+  const relationships = [];
+  const edges = [];
+
+  const relationshipArray = [];
+  const arrayItems = [];
+
+
+
 
   const radioButtons = [
     [0, "получить все узлы, связанные с отдельным узлом конкретным соотношением"],
@@ -19,7 +104,8 @@ function ExpertSystem() {
             Создание узлов
           </div>
           <label>
-            Название: <input className="rounded-2xl p-2 mt-1 w-full" name="nodesInput" />
+            Название:
+            <input className="rounded-2xl p-2 mt-1 ml-1 w-full" name="nodesInput" id="nodesInput"/>
           </label>
           <button className="p-3 bg-amber-100 rounded-2xl">Создать узел</button>
         </div>
@@ -38,21 +124,21 @@ function ExpertSystem() {
           </div>
           <label>
             Узел (от):
-            <select name="startNode" id="startNode" className="rounded-2xl p-2 mt-1 ml-1 w-full">
+            <select name="startNodeInputList" id="startNodeInputList" className="rounded-2xl p-2 mt-1 ml-1 w-full">
               <option value="1">1</option>
               <option value="2">2</option>
             </select>
           </label>
           <label>
             Отношение:
-            <select name="relationship" id="relationship" className="rounded-2xl p-2 mt-1 ml-1 w-full">
+            <select name="relationshipInputList" id="relationshipInputList" className="rounded-2xl p-2 mt-1 ml-1 w-full">
               <option value="1">1</option>
               <option value="2">2</option>
             </select>
           </label>
           <label>
             Узел (к):
-            <select name="finishNode" id="finishNode" className="rounded-2xl p-2 mt-1 ml-1 w-full">
+            <select name="finishNodeInputList" id="finishNodeInputList" className="rounded-2xl p-2 mt-1 ml-1 w-full">
               <option value="1">1</option>
               <option value="2">2</option>
             </select>
