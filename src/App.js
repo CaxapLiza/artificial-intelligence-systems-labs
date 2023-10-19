068b460d3b6d6215e4e97be47647021007e660ff
lizac <lizacaxap@yandex.ru>
2023-10-19 15:35:13 +0300

    Add main menu

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
 import './App.css';
+import ArtificialLifeMethod from "./components/ArtificialLifeMethod";
+import Clustering from "./components/Clustering";
+import ExpertSystem from "./components/ExpertSystem";
+import Perceptron from "./components/Perceptron";
+import GeneticAlgorithm from "./components/GeneticAlgorithm";
+
+import {useState} from "react";
 
 function App() {
+  const [task, setTask] = useState(0)
+  const tasks = [
+    [0, 'Экспертные системы'],
+    [1, 'Однослойный перцептрон'],
+    [2, 'Кластеризация алгоритмом k-средних'],
+    [3, 'Оптимизация функции генетическим алгоритмом'],
+    [4, 'Обучение агентов методом искусственной жизни']
+  ]
+
+  const renderComponent = () => {
+    switch (task) {
+      case 0:
+        return <ExpertSystem />;
+      case 1:
+        return <Perceptron />;
+      case 2:
+        return <Clustering />;
+      case 3:
+        return <GeneticAlgorithm />;
+      case 4:
+        return <ArtificialLifeMethod />;
+      default:
+        return null;
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-amber-100 mx-auto">
+      <div className="bg-amber-200 p-2.5">
+        <ul className="m-0 p-1 flex gap-3 text-center mx-auto max-w-7xl max-sm:flex-col">
+          {tasks.map((taskItem) => {
+            return(
+              <li key={taskItem[0]}>
+                <div
+                  className={`p-2 rounded-2xl ${task === taskItem[0] ? 'bg-amber-100' : ''}`}
+                  onClick={() => {
+                    setTask(taskItem[0])
+                  }}>
+                  {taskItem[1]}
+                </div>
+              </li>
+            )
+          })}
+        </ul>
+      </div>
+      <div className="max-w-7xl mx-auto">
+        {renderComponent()}
+      </div>
     </div>
   );
 }
